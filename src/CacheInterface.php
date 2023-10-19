1c893089bb892ddf51a234f4ad6b41d2caeb1725
S.Tougeiro <sidneytougeiro@gmail.com>
2023-10-19 12:05:29 -0300

    Adding caching interfaces

---
+++ src/CacheInterface.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+    namespace STDW\Contract;
+
+    use Psr\SimpleCache\CacheInterface as PsrCacheInterface;
+
+
+    interface CacheInterface extends PsrCacheInterface
+    {
+    }
