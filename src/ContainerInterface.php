558498994125c2645cbb66f61ff4d54d58ae124a
S.Tougeiro <sidneytougeiro@gmail.com>
2023-10-19 10:26:24 -0300

    Fixing container interfaces

--- src/ContainerInterface.php
+++ src/ContainerInterface.php
@@ -8,4 +8,10 @@
     interface ContainerInterface extends PsrContainerInterface
     {
         public function set(string $abstract, callable|string|null $concrete = null, bool $shareable = false): void;
+
+        public function make(string $abstract): object;
+
+        public function bind(string $abstract, callable|string|null $concrete = null): void;
+
+        public function singleton(string $abstract, callable|string|null $concrete = null): void;
     }
