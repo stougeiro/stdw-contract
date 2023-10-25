fccfe4e1260ea7c49a7a3208b1493099741d59eb
S.Tougeiro <sidneytougeiro@gmail.com>
2023-10-25 09:30:34 -0300

    Updating encryption interface (requiring key)

--- src/EncryptionInterface.php
+++ src/EncryptionInterface.php
@@ -5,6 +5,8 @@
 
     interface EncryptionInterface
     {
+        public function __construct(string $key);
+
         public function encrypt(mixed $value): string;
 
         public function decrypt(string $payload): mixed;
